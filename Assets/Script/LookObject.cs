7a2e3b0705eaea5cddf1af4ec2c7d11dabcd9c58
esterPett <e.pettinari@rbw-cgi.it>
2024-05-28 19:53:27 +0200

    Sistemazione didascalie

--- Assets/Script/LookObject.cs
+++ Assets/Script/LookObject.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class LookObject : MonoBehaviour
 {
     [SerializeField] private Transform camera;
-   
+    [SerializeField] private int lookZ;
    
     void Update()
     {
 
-        Vector3 LookVector =  new Vector3(0,0,-1); 
+        Vector3 LookVector =  new Vector3(0,0,lookZ); 
         Quaternion position = Quaternion.LookRotation(LookVector);
 
         //transform.rotation = position;
